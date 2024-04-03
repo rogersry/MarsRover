35a75692176c8deddf21c2f974f98ba3c27d9663
rogersry <11882477+rogersry@users.noreply.github.com>
2024-04-03 18:49:46 -0500

    Added second Motor to MarsRoverPi and enabled Left and Right

--- MarsRoverPi/MarsRoverPi/Handlers/NSB/MoveRoverHandler.cs
+++ MarsRoverPi/MarsRoverPi/Handlers/NSB/MoveRoverHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MarsRoverMessages;
+using Microsoft.Azure.Amqp.Transaction;
 using NServiceBus;
 using System.Device.Gpio;
 
@@ -7,9 +8,16 @@ namespace MarsRoverPi.Handlers.NSB
 
     public class MoveRoverHandler : IHandleMessages<MoveRoverMessage>, IDisposable
     {
-        private readonly int _motorPin1 = 17;
-        private readonly int _motorPin2 = 18;
-        private readonly int _motorEnable = 27;
+        private readonly int _motorLeftPin1 = 17;
+        private readonly int _motorLeftPin2 = 18;
+        private readonly int _motorLeftEnable = 27;
+        private readonly Motor _leftMotor;
+        private readonly Motor _rightMotor;
+
+        private readonly int _motorRightPin1 = 21;
+        private readonly int _motorRightPin2 = 16;
+        private readonly int _motorRightEnable = 25;
+
         private readonly GpioController _controller;
 
         private readonly ILogger<MoveRoverHandler> _logger;
@@ -17,13 +25,10 @@ namespace MarsRoverPi.Handlers.NSB
         public MoveRoverHandler(ILogger<MoveRoverHandler> logger)
         {
             _logger = logger;
-
             _controller = new GpioController();
-            _controller.OpenPin(_motorPin1, PinMode.Output);
-            _controller.OpenPin(_motorPin2, PinMode.Output);
-            _controller.OpenPin(_motorEnable, PinMode.Output);
-
-            _controller.Write(_motorEnable, PinValue.Low);
+            _leftMotor = new Motor(_motorLeftPin1, _motorLeftPin2, _motorLeftEnable, _controller);
+            _rightMotor = new Motor(_motorRightPin1, _motorRightPin2, _motorRightEnable, _controller);
+            _controller.Write(_motorLeftEnable, PinValue.Low);
         }
 
         public void Dispose()
@@ -37,41 +42,98 @@ namespace MarsRoverPi.Handlers.NSB
             switch (message.Direction)
             {
                 case Direction.Left:
+                    _logger.LogInformation("Turning Left");
+                    Parallel.Invoke(
+                        () =>
+                        {
+                            _leftMotor.Reverse(message.Milliseconds);
+                        },
+                        () =>
+                        {
+                            _rightMotor.Forward(message.Milliseconds);
+                        }
+                    );
                     break;
                 case Direction.Right:
+                    _logger.LogInformation("Turning Right");
+                    Parallel.Invoke(
+                        () =>
+                        {
+                            _leftMotor.Forward(message.Milliseconds);
+                        },
+                        () =>
+                        {
+                            _rightMotor.Reverse(message.Milliseconds);
+                        }
+                    );
                     break;
                 case Direction.Forward:
-                    Forward(message.Milliseconds);
+                    _logger.LogInformation("Moving Forward");
+                    Parallel.Invoke(
+                        () =>
+                        {
+                            _leftMotor.Forward(message.Milliseconds);
+                        },
+                        () =>
+                        {
+                            _rightMotor.Forward(message.Milliseconds);
+                        }
+                    );
                     break;
                 case Direction.Reverse:
-                    Reverse(message.Milliseconds);
+                    _logger.LogInformation("Moving Reverse");
+                    Parallel.Invoke(
+                        () =>
+                        {
+                            _leftMotor.Reverse(message.Milliseconds);
+                        },
+                        () =>
+                        {
+                            _rightMotor.Reverse(message.Milliseconds);
+                        }
+                    );
                     break;
             }
             _logger.LogInformation("MoveRoverHandler - Handled MoveRoverMessage.");
 
             return Task.CompletedTask;
         }
+    }
 
-        private void Forward(int milliseconds)
+    internal class Motor
+    {
+        private readonly int _motorPin1;
+        private readonly int _motorPin2;
+        private readonly int _motorEnable;
+        private readonly GpioController _controller;
+
+        public Motor(int pin1, int pin2, int pinEnable, GpioController controller)
         {
-            _logger.LogInformation("Moving Forward");
+            _motorPin1 = pin1;
+            _motorPin2 = pin2;
+            _motorEnable = pinEnable;
+            _controller = controller;
+            _controller.OpenPin(_motorPin1, PinMode.Output);
+            _controller.OpenPin(_motorPin2, PinMode.Output);
+            _controller.OpenPin(_motorEnable, PinMode.Output);
+        }
+
+        public void Forward(int milliseconds)
+        {
             _controller.Write(_motorEnable, PinValue.High);
             _controller.Write(_motorPin1, PinValue.High);
             _controller.Write(_motorPin2, PinValue.Low);
             Thread.Sleep(milliseconds);
             _controller.Write(_motorEnable, PinValue.Low);
-            _logger.LogInformation("Stopped");
         }
 
-        private void Reverse(int milliseconds)
+        public void Reverse(int milliseconds)
         {
-            _logger.LogInformation("Moving Reverse");
             _controller.Write(_motorEnable, PinValue.High);
             _controller.Write(_motorPin1, PinValue.Low);
             _controller.Write(_motorPin2, PinValue.High);
             Thread.Sleep(milliseconds);
             _controller.Write(_motorEnable, PinValue.Low);
-            _logger.LogInformation("Stopped");
         }
     }
 }
